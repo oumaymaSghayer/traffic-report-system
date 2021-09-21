8a90d9dd36685a97836bd3e2137199a6bf2b24dd
Oumayma Sghayer <extern.oumayma.sghayer@allianz.be>
2021-09-21 15:43:20 +0100

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import { TrafficInterface } from "./interfaces/traffic.interface";
 import Header from "./components/Header";
